cf05f5919c0d672bd9e0b09493c1893ede61d9cc
Emma Maddock <emmamaddock@Dickum.local>
2021-04-30 19:57:29 -0400

    fixing

--- stg/index.php
+++ stg/index.php
@@ -4,11 +4,18 @@
 
   <title>Oddball | Under Construction<title>
 
-  <link rel="stylesheet" type="text/css" href="./resources/css/home/default.css">
-  <link rel="stylesheet" type="text/css" href="./resources/css/home/responsive.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTBook.otf" as="font">
-  <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Regular.otf" as="font">
+    <link rel="stylesheet" type="text/css" href="./resources/css/fonts.css">
+    <link rel="stylesheet" type="text/css" href="./resources/css/main_default.css">
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
+
+    <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTBook.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Regular.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Thin.otf" as="font">
+
+    <script type="text/javascript" src="./resources/scripts/jquery-3.3.1.js"></script>
 
 </head>
 
