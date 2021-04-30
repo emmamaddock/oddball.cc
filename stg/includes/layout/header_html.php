35d544abee5dc3036ef7fa78d8f048609c79dfee
Emma Maddock <emmamaddock@Dickum.local>
2021-04-30 19:40:28 -0400

    trying to add resources

--- stg/includes/layout/header_html.php
+++ stg/includes/layout/header_html.php
@@ -9,35 +9,13 @@
 
   <link rel="stylesheet" type="text/css" href="/elmco/resources/css/fonts.css">
   <link rel="stylesheet" type="text/css" href="/elmco/resources/css/main_default.css">
-  <link rel="shortcut icon" type="image/x-icon" href="/elmco/favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
 
   <link rel="prefetch" href="/elmco/resources/fonts/Futura/FuturaPTBook.otf" as="font">
   <link rel="prefetch" href="/elmco/resources/fonts/proxima_sv/ProximaNova-Regular.otf" as="font">
   <link rel="prefetch" href="/elmco/resources/fonts/proxima_sv/ProximaNova-Thin.otf" as="font">
 
-  <script type="text/javascript" src="/elmco/resources/scripts/jquery-3.3.1.js"></script>
+  <script type="text/javascript" src="/../../resources/scripts/jquery-3.3.1.js"></script>
 
 </head>
 <body>
-  <div id="header-wrapper">
-    <header>
-      <div id="logo-container">
-        <a href="../../../index.php"><img src="/elmco/resources/images/logo/logo-white.png"></a>
-      </div>
-
-      <?php //TODO: make the nav depend on which pages exist?>
-        <div id="header-nav">
-          <ul>
-            <li><a class="header-item" href="/elmco/pages/about/index.php">About</a></li>
-            <li><a class="header-item" href="/elmco/pages/art/index.php">Art</a></li>
-            <li><a class="header-item" href="/elmco/pages/photography/index.php">Photography</a></li>
-            <li><a class="header-item" href="/elmco/pages/resume/index.php">Resume</a></li>
-            <li><a class="header-item" href="/elmco/pages/blog/index.php">Blog</a></li>
-            <li><a class="header-item" href="/elmco/pages/contact/index.php">Contact</a></li>
-          </ul>
-        </div>
-
-
-    </header>
-
-  </div>
--- stg/includes/layout/init.php
+++ stg/includes/layout/init.php
@@ -1,8 +1,8 @@
 <?php
   ob_start();
 
-  define("DOC_ROOT", '/var/www/elmco.ca/web/');
-  define("ADMIN_PATH",'/var/www/elmco.ca/web/admin/');
+  define("DOC_ROOT", '/oddball.cc/stg/');
+  //define("ADMIN_PATH",'/var/www/elmco.ca/web/admin/');
 
   require_once('functions.php');
   //require_once('database.php');
