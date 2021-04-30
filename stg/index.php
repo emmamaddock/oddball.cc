10903e5a46be371ecba148e06c501cccc6e410fa
Emma Maddock <emmamaddock@Dickum.local>
2021-04-30 19:53:41 -0400

    Formatting index

--- stg/index.php
+++ stg/index.php
@@ -1,6 +1,14 @@
+<!DOCTYPE html>
+<html lan="en">
 <head>
 
   <title>Oddball | Under Construction<title>
+
+  <link rel="stylesheet" type="text/css" href="./resources/css/home/default.css">
+  <link rel="stylesheet" type="text/css" href="./resources/css/home/responsive.css">
+
+  <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTBook.otf" as="font">
+  <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Regular.otf" as="font">
 
 </head>
 
