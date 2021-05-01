8f11e6af0a89a68de19d3827374e5eda315ed173
Emma Maddock <emmamaddock@Dickum.local>
2021-04-30 20:00:44 -0400

    fixed stupid bug

--- stg/index.php
+++ stg/index.php
@@ -2,7 +2,7 @@
 <html lan="en">
 <head>
 
-  <title>Oddball | Under Construction<title>
+  <title>Oddball | Under Construction</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
