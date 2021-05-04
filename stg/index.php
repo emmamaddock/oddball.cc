c4316128a714ecd616ddaabf7708a35a7f175935
Emma Maddock <emmamaddock@Dickum.local>
2021-05-04 11:26:40 -0400

    updating socials structure

--- stg/index.php
+++ stg/index.php
@@ -25,12 +25,11 @@
   <section id="construction">
     <img id="cons-logo" src="./resources/images/logo/logo.png">
     <p>Under construction, exciting things coming soon!</p>
-  </section>
-
-  <section id="socials">
-    <a href="https://www.instagram.com/o.ddball" target="_blank"><span id="social-footer-icon"><img src="./resources/images/social/instagram.png"></a></span>
-    <a href="https://www.linkedin.com/in/emma-maddock/" target="_blank"><span id="social-footer-icon"><img src="./resources/images/social/linkedin.png"></a></span>
-    <a href="https://www.github.com/emmamaddock" target="_blank"><span id="social-footer-icon"><img src="./resources/images/social/github.png"></a></span>
+    <p>
+      <a href="https://www.instagram.com/o.ddball" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/instagram.png"></a></span>
+      <a href="https://www.linkedin.com/in/emma-maddock/" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/linkedin.png"></a></span>
+      <a href="https://www.github.com/emmamaddock" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/github.png"></a></span>
+    </p>
   </section>
 
 </body>
