2c5117a7afd974b183048abff456737e92f53551
Emma Maddock <emmamaddock@Dickum.local>
2021-05-04 13:08:37 -0400

    adding socials to prod

--- index.php
+++ index.php
@@ -25,6 +25,11 @@
   <section id="construction">
     <img id="cons-logo" src="./resources/images/logo/logo.png">
     <p>Under construction, exciting things coming soon!</p>
+    <p id="socials">
+      <a href="https://www.instagram.com/o.ddball" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/instagram.png"></a></span>
+      <a href="https://www.linkedin.com/in/emma-maddock/" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/linkedin.png"></a></span>
+      <a href="https://www.github.com/emmamaddock" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/github.png"></a></span>
+    </p>
   </section>
 
 </body>
