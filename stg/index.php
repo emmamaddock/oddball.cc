2abb8b2428215960e4481e9e3adce03f39184457
Emma Maddock <emmamaddock@Dickum.local>
2021-05-04 20:23:19 -0400

    added a bunch of font resources and laid out bare structure for main page

--- stg/index.php
+++ stg/index.php
@@ -7,14 +7,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--TODO: put this in an init file-->
     <link rel="stylesheet" type="text/css" href="./resources/css/fonts.css">
     <link rel="stylesheet" type="text/css" href="./resources/css/main_default.css">
-    <link rel="stylesheet" type="text/css" href="./resources/css/construction/default.css">
+    <link rel="stylesheet" type="text/css" href="./resources/css/home/default.css">
     <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
 
+    <!--TODO: see if this is even needed
     <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTBook.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTBookOblique.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTMedium.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/Futura/FuturaPTMediumOblique.otf" as="font">
+
     <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Regular.otf" as="font">
     <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Thin.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/proxima_sv/ProximaNova-Semibold.otf" as="font">
+
+    <link rel="prefetch" href="./resources/fonts/APOLLO/APOLLO.otf" as="font">
+    <link rel="prefetch" href="./resources/fonts/APOLLO/APOLLOItalic.otf" as="font">
+
+    <link rel="prefetch" href="./resources/fonts/Caviar_Dreams/CaviarDreams.ttf" as="font">
+    <link rel="prefetch" href="./resources/fonts/Caviar_Dreams/CaviarDreams_Italic.ttf" as="font">
+    <link rel="prefetch" href="./resources/fonts/Caviar_Dreams/CaviarDreams_Bold.ttf" as="font">
+    <link rel="prefetch" href="./resources/fonts/Caviar_Dreams/CaviarDreams_BoldItalic.ttf" as="font">
+    -->
 
     <script type="text/javascript" src="./resources/scripts/jquery-3.3.1.js"></script>
 
@@ -22,14 +38,17 @@
 
 <body>
 
-  <section id="construction">
+  <section id="main-page-nav">
     <img id="cons-logo" src="./resources/images/logo/logo.png">
-    <p>Under construction, exciting things coming soon!</p>
-    <p id="socials">
-      <a href="https://www.instagram.com/o.ddball" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/instagram.png"></a></span>
-      <a href="https://www.linkedin.com/in/emma-maddock/" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/linkedin.png"></a></span>
-      <a href="https://www.github.com/emmamaddock" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/github.png"></a></span>
-    </p>
+    <nav>
+      <ul class="link-list">
+        <li><h1 class="main-page-link">ABOUT</h1></li>
+        <li><h1 class="main-page-link">RESUME</h1></li>
+        <li><h1 class="main-page-link">GALLERY</h1></li>
+        <li><h1 class="main-page-link">SERVICES</h1></li>
+        <li><h1 class="main-page-link">CONTACT</h1></li>
+      </ul>
+    </nav>
   </section>
 
 </body>
---
+++ stg/pages/construction.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lan="en">
+<head>
+
+  <title>Oddball | Under Construction</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+  <section id="construction">
+    <img id="cons-logo" src="./resources/images/logo/logo.png">
+    <p>Under construction, exciting things coming soon!</p>
+    <p id="socials">
+      <a href="https://www.instagram.com/o.ddball" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/instagram.png"></a></span>
+      <a href="https://www.linkedin.com/in/emma-maddock/" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/linkedin.png"></a></span>
+      <a href="https://www.github.com/emmamaddock" target="_blank"><span id="social-footer-icon"><img src="./resources/images/socials/github.png"></a></span>
+    </p>
+  </section>
+
+</body>
+</html>
