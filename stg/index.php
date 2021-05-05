92d6694d29ba7c780d1b9f0c3bac9790998cc244
Emma Maddock <emmamaddock@Dickum.local>
2021-05-04 22:32:34 -0400

    changed page name

--- stg/index.php
+++ stg/index.php
@@ -2,7 +2,7 @@
 <html lan="en">
 <head>
 
-  <title>Oddball | Under Construction</title>
+  <title>Oddball | Home</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
